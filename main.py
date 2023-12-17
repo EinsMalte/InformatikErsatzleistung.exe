c1fbf168c63da830cd8ce4751498db8812f80d39
Malte <malteturtle@gmail.com>
2023-12-17 19:07:42 +0100

    Refactor variable names and improve code readability

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 # Aufgabe 1 der Klausurersatzleistung mit Herrn Grünke
 
 # Importe
-import math
-import pygame
-from pygame import mixer
+import math  # Mathematische Funktionen wie z.B. Wurzel
+import pygame  # Pygame für die GUI
+from pygame import mixer  # Pygame für die Musik
 
-w, h = 800, 600  # Fenstergröße
+breite, höhe = 800, 600  # Fenstergröße
 
 
 # isPrime Funktion
@@ -40,25 +40,25 @@ def isPrime(n, verbose=False):
 
 
 # Code für pygame als GUI
-entry_text = ""
-entryPrime = False
-running = True
+nutzereingabe = ""  # Text in der Eingabe
+eingabe_ist_primzahl = False  # Ist die Eingabe eine Primzahl?
+programm_läuft = True  # Läuft das Programm?
 
 pygame.init()
-screen = pygame.display.set_mode((w, h))
-pygame.display.set_caption("Number Input")
+screen = pygame.display.set_mode((breite, höhe))
+pygame.display.set_caption("Ist es eine Primzahl.png")
 
 background_neutral = pygame.image.load("MathemannNEUTRAL.png")
-background_neutral = pygame.transform.scale(background_neutral, (w, h))
+background_neutral = pygame.transform.scale(background_neutral, (breite, höhe))
 
 background_happy = pygame.image.load("MathemannHAPPY.png")
-background_happy = pygame.transform.scale(background_happy, (w, h))
+background_happy = pygame.transform.scale(background_happy, (breite, höhe))
 
 background_sad = pygame.image.load("MathemannSAD.png")
-background_sad = pygame.transform.scale(background_sad, (w, h))
+background_sad = pygame.transform.scale(background_sad, (breite, höhe))
 
-entry = pygame.Rect(0, 0, w - 100, 80)
-entry.center = (w // 2, h - 100)
+entry = pygame.Rect(0, 0, breite - 100, 80)
+entry.center = (breite // 2, höhe - 100)
 
 clock = pygame.time.Clock()
 
@@ -68,46 +68,49 @@ mixer.music.play(-1)
 mixer.music.set_volume(.2)
 
 def onBackspace():
-    global entry_text, entryPrime
-    if len(entry_text) > 0:
-        entry_text = entry_text[:-1]
-        if len(entry_text) == 0:
-            entryPrime = False
+    global nutzereingabe, eingabe_ist_primzahl
+    if len(nutzereingabe) > 0:
+        nutzereingabe = nutzereingabe[:-1]
+        if len(nutzereingabe) == 0:
+            eingabe_ist_primzahl = False
         else:
-            entryPrime = isPrime(int(entry_text))
+            eingabe_ist_primzahl = isPrime(int(nutzereingabe))
 
 
 def drawCall():
-    global running, entry_text, entryPrime, entry, background_neutral, background_happy, background_sad, clock, screen
+    global programm_läuft, nutzereingabe, eingabe_ist_primzahl, entry, background_neutral, background_happy, background_sad, clock, screen
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            programm_läuft = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
                 onBackspace()
             else:
                 # Check if the key is a number
                 if event.unicode.isnumeric():
-                    entry_text += event.unicode
-                    entryPrime = isPrime(int(entry_text))
+                    nutzereingabe += event.unicode
+                    eingabe_ist_primzahl = isPrime(int(nutzereingabe))
 
-    if len(entry_text) == 0:
+    if len(nutzereingabe) == 0:
+        mixer.music.set_volume(.2) # Leiser wenn nichts eingegeben ist
         screen.blit(background_neutral, (0, 0))  # Neutral
-    elif entryPrime:
+    elif eingabe_ist_primzahl:
+        mixer.music.set_volume(.2) # Leiser wenn richtig ist
         screen.blit(background_happy, (0, 0))  # Happy
     else:
+        mixer.music.set_volume(1) # Lauter wenn falsch ist
         screen.blit(background_sad, (0, 0))  # Sad
     pygame.draw.rect(screen, (255, 255, 255), entry)  # Draw the entry box
 
-    if entryPrime:
+    if eingabe_ist_primzahl:
         pygame.draw.rect(screen, (0, 255, 0), entry, 5)  # Draw the border of the entry box
-    elif len(entry_text) > 0:
+    elif len(nutzereingabe) > 0:
         pygame.draw.rect(screen, (255, 0, 0), entry, 5)  # Draw the border of the entry box
     else:
         pygame.draw.rect(screen, (0, 0, 0), entry, 5)
     font = pygame.font.Font(None, 110)
-    text_surface = font.render(entry_text, True, (0, 0, 0))
-    screen.blit(text_surface, (w // 2 - text_surface.get_size()[0] // 2, entry.y + 5))
+    text_surface = font.render(nutzereingabe, True, (0, 0, 0))
+    screen.blit(text_surface, (breite // 2 - text_surface.get_size()[0] // 2, entry.y + 5))
 
     # Titel in Rainbow Farben
     font = pygame.font.Font(None, 90)
@@ -117,13 +120,13 @@ def drawCall():
         int(math.sin(pygame.time.get_ticks() / 1000 + 2) * 127 + 128),
         int(math.sin(pygame.time.get_ticks() / 1000 + 4) * 127 + 128))
     text_surface = font.render("Ist es eine Primzahl?", True, rainbowColor)
-    screen.blit(text_surface, (w // 2 - text_surface.get_size()[0] // 2, h-200))
+    screen.blit(text_surface, (breite // 2 - text_surface.get_size()[0] // 2, höhe-200))
 
     pygame.display.flip()
     clock.tick(60)
 
 
 # Main Loop
-while running:
+while programm_läuft:
     drawCall()
 pygame.quit()
